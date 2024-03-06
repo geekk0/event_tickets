5974f9a7c5c495fb5ae756834f861cb16341bc9c
gekk0 <gekk0dw@gmail.com>
2024-03-06 11:20:28 +0300

    tg chunks

--- tickets_checker/tg_bot.py
+++ tickets_checker/tg_bot.py
@@ -2,7 +2,7 @@ import telebot
 
 from os import environ
 # from dotenv import load_dotenv
-
+MAX_MESSAGE_LENGTH = 4096
 
 class TelegramBot:
     # load_dotenv()
@@ -15,6 +15,13 @@ class TelegramBot:
     def notify_admin_folder_ready(self, folder, download_url):
         message = f'Папка {folder} доступна для скачивания по ссылке: {download_url}'
         self.bot.send_message(self.chat_id, message)
+
+    def send_all_tickets(self, message_content):
+        message_chunks = [message_content[i:i + MAX_MESSAGE_LENGTH] for i in
+                          range(0, len(message_content), MAX_MESSAGE_LENGTH)]
+        for chunk in message_chunks:
+            # Send the chunk as a message using TelegramApi
+            self.send_message(chunk)
 
     def send_message(self, message):
         self.bot.send_message(self.chat_id, message)
